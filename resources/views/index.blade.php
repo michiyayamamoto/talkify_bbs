34777963489dc8b45a54765a3dd174cc2fe90d91
EC2 Default User <ec2-user@ip-172-31-16-44.ec2.internal>
2023-04-03 02:51:17 +0000

    0403update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
 <div class="border my-2 p-2">
     <div class="text-secondary">{{ $topic->name }} さん</div>
     <div class="p-2">{!! nl2br(e($topic->content)) !!}</div>
+    <div class="text-secondary">コメント:({{ $topic->comments->count() }})</div>
     <div class="text-secondary">投稿日:{{ $topic->created_at }}</div>
     <div class="text-secondary">編集日:{{ $topic->updated_at }}</div>
     <a class="btn btn-outline-primary" href="{{ route('topics.show',$topic->id) }}">詳細</a>
